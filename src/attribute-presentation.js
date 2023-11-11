3ead27d5779bd0d9a901ce6b42400da26dff9e96
Egor Ermolaev <egor.ermolaev@uni.lu>
2023-11-11 16:26:01 +0100

    Hardcode challenge for the z-commerce demo

--- src/attribute-presentation.js
+++ src/attribute-presentation.js
@@ -1,23 +1,28 @@
 const {isConnectionEstablished} = require("./connection");
+
+
+const AttributePresentation = {challenge: 14612461246, expiration: 12}//TODO make a collection in mongo, retrieve by Proof Request id for each user
+
 const requestAttributePresentation = (orderID) => {
     console.debug("Requesting Attribute Presentation by orderID, orderID >>", orderID)
     isConnectionEstablished(orderID);
     //TODO this func should trigger a pop up in the Wallet via web hook, when connection is established
+
+    return AttributePresentation
 }
 
 const verifierURL = process.env.HEIMDALL_VERIFIER_URL
 const fs = require('fs').promises;
 const path = require('path');
-const { exec } = require('child_process');
+const {exec} = require('child_process');
 
 //TODO something goes wrong here with the file uploading, it's a temp solution to upload the files, it does not support long filenames etc.
 const uploadAttributePresentationToVerifierHeimdall = async (fileName, attributePresentation) => {
-    const tempFilePath = path.join(__dirname, fileName);
+    const tempFilePath = "/app/tmp/" + fileName;
     await fs.writeFile(tempFilePath, JSON.stringify(attributePresentation));
-    const curlCommand = `curl -s --request POST "http://${verifierURL}/upload/file?name=${fileName}" --form "uplfile=@${fileName}"`;
+    const curlCommand = `curl -s --request POST "http://${verifierURL}/upload/file?name=${fileName}" --form "uplfile=@${tempFilePath}"`;
 
     await exec(curlCommand, async (error, stdout, stderr) => {
-        await fs.unlink(tempFilePath)
         if (error) {
             throw error
         }
@@ -28,14 +33,16 @@ const uploadAttributePresentationToVerifierHeimdall = async (fileName, attribute
     });
 }
 
-const verifyAttributePresentation = async (orderID, attributePresentation) => {
+const verifyAttributePresentation = async (orderID, attributePresentation, ) => {
     console.debug("Verifying Attribute Presentation by orderID, orderID >>", orderID)
 
     const attributePresentationFileName = `${orderID}.json`
     await uploadAttributePresentationToVerifierHeimdall(attributePresentationFileName, attributePresentation)
 
+    const challenge = AttributePresentation.challenge//TODO request Proof Request entity here and get the preliminarily given challenge for verifivation
+
     const verificationResultFileName = `${orderID}.txt`
-    const verificationResultResponse = await fetch(`http://${verifierURL}/heimdalljs/verify?path=${attributePresentationFileName}&name=${verificationResultFileName}`)
+    const verificationResultResponse = await fetch(`http://${verifierURL}/heimdalljs/verify?path=${attributePresentationFileName}&name=${verificationResultFileName}&publicKey=issuer_pk.json&challenge=${challenge}`)
     const verificationResult = await verificationResultResponse.json()
     console.debug("Attribute Presentation verified by orderID, orderID >>", orderID)
     console.debug("Verification result, verificationResult >>", verificationResult)
